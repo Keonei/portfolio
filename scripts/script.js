00f915337a09a0e3847bf7d89a8720af8fc8fb2f
kerry <keonei@uw.edu>
2017-06-26 01:27:57 -0700

    add script page, add scroll top, made design edits

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+  var target = $(this.hash);
+  target = target.length ? target : $('[name=' + this.hash.substr(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+  });
+});
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 1200) {
+        $('.scrollup').fadeIn(200);
+    } else {
+        $('.scrollup').fadeOut(200);
+    }
+});
+$('.scrollup').click(function() {
+    $('body,html').animate({
+        scrollTop : 0
+    }, 500);
+});
